7b69ef441541bfde1e6caaeb2e5d14ae7348c3ef
Amaroke <hugo.amaroke@gmail.com>
2023-09-18 13:19:01 +0200

    clear useless backend

--- backend/src/models/person.schema.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const personSchema = new Schema({
-    firstname: String,
-    lastname: String,
-    entity: String,
-});
-
-export const PersonModel = model('Person', personSchema);
