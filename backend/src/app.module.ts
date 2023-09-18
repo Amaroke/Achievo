3f3839035db32c743b744affcc54adb378d4e534
Amaroke <hugo.amaroke@gmail.com>
2023-09-18 13:19:01 +0200

    clear useless backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { PersonModule } from './person/person.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/achievo'),
-    PersonModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
