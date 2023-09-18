5e954f2da4d8dfe7e6a2f97965327fbad4a7848b
Amaroke <hugo.amaroke@gmail.com>
2023-09-18 13:05:05 +0200

    Frontend default components initialization

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MainSectionComponent } from './main-section/main-section.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainSectionComponent, FooterComponent, NavigationBarComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
